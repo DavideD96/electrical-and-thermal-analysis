620be3855fd3fd092ee8a4c91816118736cbb3a8
Cri690 <cristinazuccali@virgilio.it>
2023-10-11 10:56:56 +0200

    Create prova.m

---
+++ prova.m
@@ -0,0 +1,9 @@
+x = {linspace(-2,3,51),linspace(-3,3,61)};
+[xx,yy] = ndgrid(x{1},x{2}); 
+y = peaks(xx, yy);
+noisy = y + (rand(size(y)) - 0.5);
+figure
+surf(xx,yy,noisy)
+[sval,p] = csaps(x,noisy,[],x);
+figure
+surf(x{1},x{2},sval.')
