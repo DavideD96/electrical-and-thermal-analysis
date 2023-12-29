9b901d4e4fa10dea7f3b1b337a15a9d548fd5ba9
DavideD96 <davidedecastri@gmail.com>
2023-12-29 18:25:24 +0100

    update

--- pca/pca_thermal001.m
+++ pca/pca_thermal001.m
@@ -11,10 +11,8 @@ function coeff = pca_thermal001(filename)
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-
 data = load(filename,'-mat');
-data = data.mtotalEvT;
+data = cell2mat(struct2cell(data));
 nframes = size(data,3);
 [rows,col] = size(data,1,2);
 data_ = zeros(nframes,rows*col);
@@ -39,66 +37,105 @@ clear data
     
 pcaSignificance = explained;
 
-plot(linspace(1,10,10), pcaSignificance(1:10));
+weigths = figure;
+plot(linspace(1,12,12), pcaSignificance(1:12));
 xlabel('pca component');
 ylabel('variance %');
-set(gca, 'XTick', 1:10)
+grid on;
+set(gca, 'XTick', 1:12)
 
-%plot eigenvalues of covariance matrix
-figure
-plot(linspace(1,10,10), latent(1:10));
-xlabel('pca component');
-ylabel('latent');
-set(gca, 'XTick', 1:10)
+% %plot eigenvalues of covariance matrix
+% figure
+% plot(linspace(1,10,10), latent(1:10));
+% xlabel('pca component');
+% ylabel('latent');
+% set(gca, 'XTick', 1:10)
 
+PC = figure;
+
+subplot(2,3,1);
+hold on;
 %first principal component
 pc1 = reshape(coeff(:,1),[rows,col]);
-figure
 title('pc1');
 imagesc(pc1);
+xlim([1,col]);
+ylim([1,rows]);
 colorbar
+hold off;
 
+subplot(2,3,2);
+hold on;
+title('pc2');
 %second principal component
 pc2 = reshape(coeff(:,2),[rows,col]);
-figure
-title('pc2');
 imagesc(pc2);
+xlim([1,col]);
+ylim([1,rows]);
 colorbar
+hold off;
 
+subplot(2,3,3);
+hold on;
 %third principal component
 pc3 = reshape(coeff(:,3),[rows,col]);
-figure
 title('pc3');
 imagesc(pc3);
+xlim([1,col]);
+ylim([1,rows]);
 colorbar
+hold off;
 
+subplot(2,3,4);
+hold on;
 %fourth principal component
 pc4 = reshape(coeff(:,4),[rows,col]);
-figure
 title('pc4');
 imagesc(pc4);
+xlim([1,col]);
+ylim([1,rows]);
 colorbar
+hold off;
 
+subplot(2,3,5);
+hold on;
 %fifth principal component
 pc5 = reshape(coeff(:,5),[rows,col]);
-figure
 title('pc5');
 imagesc(pc5);
+xlim([1,col]);
+ylim([1,rows]);
 colorbar
+hold off;
 
-%7th principal component
-pc7 = reshape(coeff(:,7),[rows,col]);
-figure
-title('pc7');
-imagesc(pc7);
+subplot(2,3,6);
+hold on;
+%6th principal component
+pc6 = reshape(coeff(:,6),[rows,col]);
+title('pc6');
+imagesc(pc6);
+xlim([1,col]);
+ylim([1,rows]);
 colorbar
+hold off;
 
+sgtitle('first principal components')
 
-%reconstruct frame 1
-fr1 = reshape(scores(1,:)*coeff'+ repmat(mu1,1,1),[rows,col]); %i dati sono centrati, il numero di gradi di libertà è n-1
-figure
-title('fr1');
-imagesc(fr1);
-colorbar
+% figure
+% %7th principal component
+% pc7 = reshape(coeff(:,7),[rows,col]);
+% title('pc7');
+% imagesc(pc7);
+% colorbar
 
+% %reconstruct frame 1
+% fr1 = reshape(scores(1,:)*coeff'+ repmat(mu1,1,1),[rows,col]); %i dati sono centrati, il numero di gradi di libertà è n-1
+% figure
+% title('fr1');
+% imagesc(fr1);
+% colorbar
+% 
+
+savefig(PC, append(filename,'_PC_','.fig'));
+savefig(weigths, append(filename,'_pesi_','.fig'));
 end
