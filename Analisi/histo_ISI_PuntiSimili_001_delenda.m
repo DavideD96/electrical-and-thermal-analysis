edb4fd00b6db246a0c91548af1480bdf2beb25b0
DavideD96 <davidedecastri@gmail.com>
2024-03-13 01:52:11 +0100

    Update histo_ISI_PuntiSimili_001_delenda.m

--- Analisi/histo_ISI_PuntiSimili_001_delenda.m
+++ Analisi/histo_ISI_PuntiSimili_001_delenda.m
@@ -1,4 +1,4 @@
-function [center, altezze] = histo_ISI_PuntiSimili_001_delenda(intervals1, intervals2)
+function histo_ISI_PuntiSimili_001_delenda(intervals1, intervals2)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %24/01/2024
 %cerca_punti_simili001(massimi, minimi) e associa evento resistivo
@@ -46,10 +46,29 @@ hold on
 
     centre_for_fit = center(altezze ~= 0);
     altezze_for_fit = altezze(altezze ~= 0);
-    centre_for_fit = centre_for_fit(1:9);
-    altezze_for_fit = altezze_for_fit(1:9);
-    lincoeff = polyfit(log10(centre_for_fit(1:5)), log10(altezze_for_fit(1:5)), 1);
-    fit_data = centre_for_fit.^lincoeff(1)*10^lincoeff(2);
+    centre_for_fit = centre_for_fit(1:7);
+    altezze_for_fit = altezze_for_fit(1:7);
+    %lincoeff = polyfit(log10(centre_for_fit(1:5)), log10(altezze_for_fit(1:5)), 1);
+    %fit_data = centre_for_fit.^lincoeff(1)*10^lincoeff(2); 
+    ft = fittype( 'lambda*exp(-(x*lambda))', 'independent', 'x', 'dependent', 'y' );
+    opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
+    opts.StartPoint = 1;
+    [fitresult, gof] = fit( centre_for_fit', altezze_for_fit', ft, opts );
+    h = plot( fitresult, center, altezze);
+    gof
+
+    ft = fittype( 'A*(x)^(-n)', 'independent', 'x', 'dependent', 'y' );
+    opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
+    opts.StartPoint = [1 2];
+    [fitresult, gof] = fit( centre_for_fit', altezze_for_fit', ft, opts );
+    h = plot( fitresult, centre_for_fit, altezze_for_fit );
+    fit_data = centre_for_fit.^(-fitresult(2))*fitresult(1); 
+    loglog(center, altezze);
+    hold on
+    loglog(centre_for_fit, fit_data);
+    hold off
+    gof
+
 %     ft = fittype( '(x+x0).^n', 'independent', 'x', 'dependent', 'y' );
 %     opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
 %     opts.StartPoint = [10 2];
@@ -58,8 +77,8 @@ hold on
 % % Fit model to data.
 % [fitresult, gof] = fit( centre_for_fit', altezze_for_fit', ft, opts );
 % h = plot( fitresult, centre_for_fit, altezze_for_fit );
-hold on
-plot(centre_for_fit,fit_data,'r');
+%hold on
+%plot(centre_for_fit,fit_data,'r');
 
 hold off
 
