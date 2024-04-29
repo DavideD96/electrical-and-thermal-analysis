9efa035165f073017d59c47cebbf36573ce0b938
DavideD96 <davidedecastri@gmail.com>
2024-04-29 09:24:03 +0200

    main tomography

--- ThermalTtomography/test tomography/setInitialRefPoint.m
+++ ThermalTtomography/test tomography/setInitialRefPoint.m
@@ -24,4 +24,32 @@ matfilename=sprintf(append(filename,num2str(frame),'_CAF_coordinates.mat'));
 save(matfilename,'coordinates','filename');
 save(append(filename,num2str(frame),'_ref1_coordinates.mat'),'ref_coordinates','filename');
 
+%set grooves
+figure
+imagesc(m(coordinates(1,2):coordinates(3,2),coordinates(1,1):coordinates(3,1)));
+colormap("parula");
+title('set a-b/1-2 groove');
+pause
+[x,y] = ginput(2); %ref1, respect to new boundaries! ref2 (avoid rotations)
+groove1_coordinates = [x,y]; %Rab 
+groove1_coordinates = [groove1_coordinates;x(2),y(2)];
+groove1_coordinates(2,:) = [x(2),y(1)];
+save(append(filename,num2str(frame),'_groove_a-b_3-6_coordinates.mat'),'groove1_coordinates','filename');
+ 
+title('set b-c/2-3 groove');
+pause
+[x,y] = ginput(2); %ref1, respect to new boundaries! ref2 (avoid rotations)
+groove2_coordinates = [x,y]; %Rab 
+groove2_coordinates = [groove2_coordinates;x(2),y(2)];
+groove2_coordinates(2,:) = [x(2),y(1)];
+save(append(filename,num2str(frame),'_groove_b-c_3-8_coordinates.mat'),'groove2_coordinates','filename');
+
+title('set c-a/1-3 groove');
+pause
+[x,y] = ginput(2); %ref1, respect to new boundaries! ref2 (avoid rotations)
+groove3_coordinates = [x,y]; %Rab 
+groove3_coordinates = [groove3_coordinates;x(2),y(2)];
+groove3_coordinates(2,:) = [x(2),y(1)];
+save(append(filename,num2str(frame),'_groove_c-a_2-8_coordinates.mat'),'groove3_coordinates','filename');
+ 
 end
